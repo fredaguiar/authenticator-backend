ab4920061d9368c0f1490d9d3c5845ab1b9a9b4b
Fred <fred.aguiar.teixeira@hotmail.com>
2024-03-01 16:57:40 -0800

    signup working

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -65,7 +65,6 @@ export const resolvers = {
       context: ApolloServerContext
     ) {
       const { req, res } = context;
-
       const existingUser = await User.findOne<TUser>({ email }).exec();
       if (existingUser) {
         throw new GraphQLError('User already exists', {
@@ -74,10 +73,14 @@ export const resolvers = {
       }
 
       try {
+        console.log('newUser NOW!!!!');
         const newUser = await User.create<TUser>({ name, email, password });
+        console.log('newUser', newUser.password);
         const token = generateToken(newUser, res);
+        console.log('token', token);
         return { name: newUser.name, email: newUser.email, token };
       } catch (err: any) {
+        console.log('err.stack', err.stack);
         throw new GraphQLError('Sign-up user error', {
           extensions: { code: 'SIGUN_UP_ERROR', message: err.message },
         });
@@ -90,31 +93,21 @@ export const resolvers = {
       context: ApolloServerContext
     ) {
       const { req, res } = context;
-
-      console.log('email, password', email, password);
-
-      try {
-        const user = await User.findOne<TUser>({ email }).exec();
-        if (!user) {
-          throw new GraphQLError('User does not exist', {
-            extensions: { code: 'USER_NOT_EXIST', email },
-          });
-        }
-        const auth = await bcrypt.compare(password, user.password);
-        if (!auth) {
-          throw new GraphQLError('Invalid username or password', {
-            extensions: { code: 'USER_INVALID_USERNAME_PASSWORD', email },
-          });
-        }
-
-        const token = generateToken(user, res);
-
-        return { name: user.name, email: user.email, token };
-      } catch (err: any) {
-        throw new GraphQLError('Login error', {
-          extensions: { code: 'USER_LOGIN_ERROR', email },
+      const user = await User.findOne<TUser>({ email }).exec();
+      console.log('user DB', user);
+      if (!user) {
+        throw new GraphQLError('User does not exist', {
+          extensions: { code: 'USER_NOT_EXIST', email },
         });
       }
+      const auth = await bcrypt.compare(password, user.password);
+      if (!auth) {
+        throw new GraphQLError('Invalid username or password', {
+          extensions: { code: 'USER_INVALID_USERNAME_PASSWORD', email },
+        });
+      }
+      const token = generateToken(user, res);
+      return { name: user.name, email: user.email, token };
     },
   },
 };
